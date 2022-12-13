dccd4ff16978c1b8c7053060b9b9d04b9e6a5841
yousuf3579 <120466678+yousuf3579@users.noreply.github.com>
2022-12-13 15:54:09 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+console.log("hello");
+console.log("my name is Yousuf");
+console.log("I am 13 years old");
+console.log("I like playing football");
+console.log("my favorite hobby is playing video games");
+console.log("I live with my family of six");
+console.log("I have a little brother");
+console.log("I have two sisters");
+console.log("I like going out to the mall with my friends");
